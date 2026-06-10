827ce438d42f259bbf0f18e4258364ff3beda30b
Луковников Михаил <m.lukovnikov@domain.local>
2026-06-10 17:18:35 +0300

    checks

--- main.js
+++ main.js
@@ -14,6 +14,18 @@ app.engine(
     "handlebars",
     engine({
         partialsDir: "src/views/partials",
+        helpers: {
+            formatDate(date) {
+                const d = new Date(date);
+                const day = String(d.getDate()).padStart(2, "0");
+                const month = String(d.getMonth() + 1).padStart(2, "0");
+                const year = d.getFullYear();
+                const hours = String(d.getHours()).padStart(2, "0");
+                const minutes = String(d.getMinutes()).padStart(2, "0")
+                
+                return `${day}.${month}.${year} ${hours}:${minutes}`
+            }
+        }
     })
 );
 app.set("view engine", "handlebars");
