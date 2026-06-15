038b5126c46db1e92f30c85ba97b2eadee9fcaf7
Луковников Михаил <m.lukovnikov@domain.local>
2026-06-15 16:16:25 +0300

    final

--- src/database/db.js
+++ src/database/db.js
@@ -1,11 +1,12 @@
 import pg from "pg";
+import "dotenv/config";
 
 const { Pool } = pg;
 
 export const pool = new Pool({
-    user: "postgres",
-    host: "localhost",
-    database: "demoexam",
-    password: "12345",
-    port: 5432,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: +process.env.DB_PORT,
 });
